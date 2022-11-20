74a9b7c7d90330aefb18a1ee5641017df6fca098
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-20 22:19:15 +0700

    coba dropdown

--- asu.py
+++ asu.py
@@ -3,7 +3,14 @@
 #       return rmse
 # asu=kampret('asu')
 # print(asu)
-import streamlit as asu
-asu.text('asu')
-a = asu.text_input('asu')
-asu.write(a)
+import streamlit as st
+# asu.text('asu')
+# a = asu.text_input('asu')
+# asu.write(a)
+
+# dropdown
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
+
+ st.write('You selected:', option)
