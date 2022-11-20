2955910e9cd18ddd508b3574b93e249e58487359
Alief Akbar Purnama <74760595+aliefakbrp@users.noreply.github.com>
2022-11-20 22:21:53 +0700

    Update asu.py

--- asu.py
+++ asu.py
@@ -12,5 +12,4 @@ import streamlit as st
 option = st.selectbox(
      'How would you like to be contacted?',
      ('Email', 'Home phone', 'Mobile phone'))
-
- st.write('You selected:', option)
+st.write('You selected:', option)
